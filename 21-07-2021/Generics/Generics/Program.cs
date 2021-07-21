21321f63982b818ab22b9dab74994b074cc7e41b
Srividhya26 <64851602+Srividhya26@users.noreply.github.com>
2021-07-21 09:31:09 -0700

    Add files via upload

---
+++ 21-07-2021/Generics/Generics/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace Generics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var students = new StudentList<int, string>();
+            students.Add(11, "sri");
+            students.Add(12, "Suji");
+            students.Add(13, "Manoj");
+            students.Add(14, "Mahesh");
+            students.Add(15, "Sunitha");
+
+            students.Remove("Sunitha");
+
+            students.Sort(students.ToString());
+
+        }
+
+        public class StudentList<T, T1>
+        {
+            public void Add(T id,T1 name)
+            {
+                Console.WriteLine("ID : " + id + " Name : " + name);
+            }
+
+            public void Remove(T1 name)
+            {
+                Console.WriteLine($"The {name} was removed from the Generic Dictionary");
+            }
+
+            public void Sort(T1 name)
+            {
+                Console.WriteLine("The values are sorted");
+            }
+        }
+    }
+    
+}
---
+++ 21-07-2021/LINQ/LINQ/Bank.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LINQ
+{
+    class Bank
+    {
+        static void Main(string[] args)
+        {
+            var branches = new List<Branches> 
+            { 
+                new Branches { BankName = "ICICI" , PhoneNumber = 9647534637 , BranchLocation = "Anna nagar",MaxPoints = 90 },
+                new Branches { BankName = "IOB" , PhoneNumber = 9879038477 , BranchLocation = "Chrompet", MaxPoints = 89 },
+                new Branches { BankName = "SBI" , PhoneNumber = 9885382230 , BranchLocation = "KK Nagar", MaxPoints = 75 },
+                new Branches { BankName = "KOB" , PhoneNumber = 9940232176 , BranchLocation = "Pammal", MaxPoints = 85 },
+                new Branches { BankName = "Indian Bank" , PhoneNumber = 8414123990 , BranchLocation = "Anna nagar", MaxPoints = 80 }
+            };
+
+            var managers = new List<Managers> 
+            { 
+                new Managers { ManagerId = 101 , ManagerName = "Sai" , ManagerBankName = "ICICI",Age = 32},
+                new Managers { ManagerId = 102 , ManagerName = "Jeni", ManagerBankName = "IOB",Age = 28},
+                new Managers { ManagerId = 103 , ManagerName = "Deva", ManagerBankName = "SBI",Age = 29},
+                new Managers { ManagerId = 104 , ManagerName = "Divya", ManagerBankName = "SBI",Age = 35},
+                new Managers { ManagerId = 105 , ManagerName = "Sri", ManagerBankName = "Indian Bank",Age = 30}
+            };
+
+            var join = branches.Join(managers,
+                                      b => b.BankName,
+                                      m => m.ManagerBankName,
+                                      (b,m) => new
+                                      {
+                                          BranchBankName = b.BankName,
+                                          ManagerBankName = m.ManagerBankName,
+                                          ManagerName = m.ManagerName,
+                                          Age = m.Age,
+                                          MaximumPoints = b.MaxPoints,
+                                      });
+            Console.WriteLine("To display branches and respective managers \n");
+
+            foreach (var name in join)
+            {
+                Console.WriteLine("Branch Name : " + name.BranchBankName + ", Manager Name : " + name.ManagerName);
+            }
+
+            Console.WriteLine("\nThe Managers aged above 30 \n");
+
+            foreach (var age in join)
+            {
+                if (age.Age > 30)
+                {
+                    Console.WriteLine("Manager Name : " + age.ManagerName + ", Bank Name : " + age.BranchBankName);
+                }
+
+            }
+
+            Console.WriteLine("\nManager who scored maximum points out of 100");
+
+            foreach(var max in join)
+            {
+                if(max.MaximumPoints == branches.Max(m => m.MaxPoints))
+                {
+                    Console.WriteLine(max.ManagerName);
+                }
+            }
+            
+      
+
+        }
+
+        public class Branches
+        {
+            public string BankName { get; set; }
+            public long PhoneNumber{ get; set; }
+            public string BranchLocation { get; set; }
+            public int MaxPoints { get; set; }
+        }
+
+        public class Managers
+        {
+            public int ManagerId { get; set; }
+            public string ManagerName { get; set; }
+            public string  ManagerBankName { get; set; }
+            public int Age { get; set; }
+
+        }
+    }
+}
