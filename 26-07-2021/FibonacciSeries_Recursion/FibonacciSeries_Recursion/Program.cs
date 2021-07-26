8adc0f3d7f4fcdeb97cfbb809e476f3d95725bff
Srividhya26 <64851602+Srividhya26@users.noreply.github.com>
2021-07-26 09:07:28 -0700

    Add files via upload

---
+++ 26-07-2021/FibonacciSeries_Recursion/FibonacciSeries_Recursion/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace FibonacciSeries_Recursion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FibonacciSeries(11);
+        }
+
+        public static void FibonacciSeries(int number)
+        {
+            FibonacciSeries(0,1,1,number);
+        }
+        public static void FibonacciSeries(int a,int b,int c,int num)
+        {
+            if(c <= num)
+            {
+                Console.WriteLine(a);
+                FibonacciSeries(b, a + b, c + 1, num);
+            }
+            
+        }
+    }
+}
---
+++ 26-07-2021/StringReversal_Recursion/StringReversal_Recursion/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace StringReversal_Recursion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StringReverse("hello");
+        }
+
+        public static void StringReverse(string text)
+        {
+            int len = text.Length;   
+            if(len <= 1)
+            {
+                Console.WriteLine(text);
+            }
+            else
+            {
+                Console.Write(text[len - 1]);
+                StringReverse(text.Substring(0, (len - 1)));
+            }
+        }
+    }
+}
