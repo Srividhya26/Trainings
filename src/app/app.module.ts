6c4d184ec96b5ccb7acb3605088a04c71ea9df79
Srividhya26 <venksri8@gmail.com>
2021-08-08 20:48:13 -0700

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { todo } from './todo/todo.compoent';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    todo
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/todo/todo.compoent.ts
@@ -0,0 +1,32 @@
+import { Component } from "@angular/core";
+
+@Component({
+    templateUrl : './todo.component.html',
+    selector : 'app-todo'
+})
+
+export class todo 
+{
+    public items = []
+    
+    public addToList(item:string)
+    {
+        if(item == '')
+        {
+            alert("The task cannot be empty");
+        }
+
+        else
+        {
+            this.items.push(item);
+            item = '';
+        }
+        
+    }
+
+    public deleteItem(index)
+    {
+        this.items.splice(index,1);
+    }
+}
+
