db47640712dc442c57c311ebed70aeb5cba8efb8
Srividhya26 <64851602+Srividhya26@users.noreply.github.com>
2021-08-24 22:20:03 +0530

    Added update and delete operations.

---
+++ BankCustomer/BankCustomer/Controllers/CustomerController1.cs
@@ -0,0 +1,90 @@
+﻿using BankCustomer.Data;
+using BankCustomer.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BankCustomer.Controllers
+{
+    public class CustomerController1 : Controller
+    {
+        private readonly MyDbContext _myDb;
+
+        public CustomerController1(MyDbContext myDb)
+        {
+            _myDb = myDb;
+        }
+        public IActionResult Index()
+        {
+            IEnumerable<Customer> customerList = _myDb.Customers;
+
+            return View(customerList);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Customer cust)
+        {
+            if(ModelState.IsValid)
+            {
+                _myDb.Customers.Add(cust);
+                _myDb.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+
+            return View(cust);
+        }
+
+        public IActionResult Update(int? id)
+        {
+            if(id == null)
+            {
+                return NotFound();
+            }
+
+            var cust = _myDb.Customers.FirstOrDefault(x => x.Id == id);
+            if(cust == null)
+            {
+                return NotFound();
+            }
+
+            return View(cust);
+        }
+
+        [HttpPost]
+        public IActionResult Update(Customer cust)
+        {
+            if(ModelState.IsValid)
+            {
+                _myDb.Customers.Update(cust);
+                _myDb.SaveChanges();
+            }
+
+            ViewBag.Message = string.Format("The updation is successfull");
+
+            return View(cust);
+        }
+
+        //[HttpPost]
+        public IActionResult Delete(int? id)
+        {
+            var cust = _myDb.Customers.Find(id);
+
+            if(cust == null)
+            {
+                return NotFound();
+            }
+            _myDb.Customers.Remove(cust);
+            _myDb.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ BankCustomer/BankCustomer/Migrations/20210824033354_Customers.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BankCustomer.Migrations
+{
+    public partial class Customers : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Customers",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Address = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Age = table.Column<int>(type: "int", nullable: false),
+                    Gender = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    OpeningBalance = table.Column<double>(type: "float", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Customers", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Customers");
+        }
+    }
+}
