8650f54a1a9fd40b16d0ead08ac5af4d1b2cdad8
Srividhya26 <64851602+Srividhya26@users.noreply.github.com>
2021-08-25 23:13:38 +0530

    Employee Data Table

---
+++ 25-08-2021/EmployeeDetails/EmployeeDetails/Migrations/20210826051212_Softdelete.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EmployeeDetails.Migrations
+{
+    public partial class Softdelete : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "DelStatus",
+                table: "Employee",
+                type: "nvarchar(1)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DelStatus",
+                table: "Employee");
+        }
+    }
+}
