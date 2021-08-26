78a01eb17060740b23061a693bcfdf3f3d9596e3
Srividhya26 <64851602+Srividhya26@users.noreply.github.com>
2021-08-26 22:51:11 +0530

    Add files via upload

---
+++ EmployeeDetails/EmployeeDetails/Controllers/DepartmentController.cs
@@ -0,0 +1,46 @@
+﻿using EmployeeDetails.Data;
+using EmployeeDetails.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeDetails.Controllers
+{
+    public class DepartmentController : Controller
+    {
+        private readonly MyDbContext _db;
+
+        public DepartmentController(MyDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            IEnumerable<DepartmentDetails> departments = _db.Department;
+
+            return View(departments);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        
+        public IActionResult Create(DepartmentDetails dept)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Department.Add(dept);
+                _db.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+
+            return View(dept);
+        }
+    }
+}
---
+++ EmployeeDetails/EmployeeDetails/Data/MyDbContext.cs
@@ -0,0 +1,41 @@
+﻿using EmployeeDetails.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeDetails.Data
+{
+    public class MyDbContext : DbContext
+    {
+        internal IEnumerable<Migrations.Employees> customer;
+
+        public MyDbContext(DbContextOptions<MyDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<EmployeesDetail> Employee { get; set; }
+
+        public override int SaveChanges()
+        {
+            foreach (var entry in ChangeTracker.Entries())
+            {
+                var entity = entry.Entity;
+                if (entry.State == EntityState.Deleted)
+                {
+                    entry.State = EntityState.Modified;
+
+                    entity.GetType().GetProperty("DelStatus").SetValue(entity, 'D');
+                }
+            }
+
+            return base.SaveChanges();
+        }
+
+        public DbSet<DepartmentDetails> Department { get; set; }
+
+        
+    }
+}
